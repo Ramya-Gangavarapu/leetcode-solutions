f434ec039306aca01f47e5a1d768fb3194332cd8
Ramya Gangavarapu <141222323+Ramya-Gangavarapu@users.noreply.github.com>
2023-11-02 10:36:28 +0530

    Add files via upload

---
+++ 10.regular-expression-matching.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=10 lang=python3
+#
+# [10] Regular Expression Matching
+#
+
+# @lc code=start
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        dp = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
+
+        dp[0][0] = True
+
+        for j in range(1, len(p) + 1):
+            if p[j - 1] == '*':
+                dp[0][j] = dp[0][j - 2]
+
+        for i in range(1, len(s) + 1):
+            for j in range(1, len(p) + 1):
+                if p[j - 1] == s[i - 1] or p[j - 1] == '.':
+                    dp[i][j] = dp[i - 1][j - 1]
+                elif p[j - 1] == '*':
+                    dp[i][j] = dp[i][j - 2] or (dp[i - 1][j] and (s[i - 1] == p[j - 2] or p[j - 2] == '.'))
+
+        return dp[len(s)][len(p)]
+        
+# @lc code=end
+
