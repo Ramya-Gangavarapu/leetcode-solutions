8716a0f021c2e87e5caf15a202a24130c1e677c0
Ramya Gangavarapu <141222323+Ramya-Gangavarapu@users.noreply.github.com>
2023-11-02 10:26:35 +0530

    Add files via upload

---
+++ 6.zigzag-conversion.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=6 lang=python3
+#
+# [6] Zigzag Conversion
+#
+
+# @lc code=start
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        result = ['' for _ in range(numRows)]
+        index, step = 0, 1
+
+        for char in s:
+            result[index] += char
+            if index == 0:
+                step = 1
+            elif index == numRows - 1:
+                step = -1
+            index += step
+
+        return ''.join(result)
+
+        
+# @lc code=end
+
