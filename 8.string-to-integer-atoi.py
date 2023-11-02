146d8290b3f1c0a9f5adad2f4b569fee6e70184c
Ramya Gangavarapu <141222323+Ramya-Gangavarapu@users.noreply.github.com>
2023-11-02 10:30:29 +0530

    Add files via upload

---
+++ 8.string-to-integer-atoi.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=8 lang=python3
+#
+# [8] String to Integer (atoi)
+#
+
+# @lc code=start
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        return max(min(int(*re.findall('^[\+\-]?\d+', s.lstrip())), 2**31 - 1), -2**31)
+        
+# @lc code=end
+
