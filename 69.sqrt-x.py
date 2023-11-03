648ee482ebc0682c69bafc811882c333b17ff621
Ramya Gangavarapu <141222323+Ramya-Gangavarapu@users.noreply.github.com>
2023-11-03 11:23:53 +0530

    Add files via upload

---
+++ 69.sqrt-x.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=69 lang=python3
+#
+# [69] Sqrt(x)
+#
+
+# @lc code=start
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        left, right = 1, x
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid > x:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left - 1
+        
+# @lc code=end
+
