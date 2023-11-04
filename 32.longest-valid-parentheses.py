498c0acf6cdec8f79f4e3f0dfb2ebc452483bdf5
Ramya Gangavarapu <141222323+Ramya-Gangavarapu@users.noreply.github.com>
2023-11-04 08:57:16 +0530

    Add files via upload

---
+++ 32.longest-valid-parentheses.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=32 lang=python3
+#
+# [32] Longest Valid Parentheses
+#
+
+# @lc code=start
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        stack = []
+        max_length = 0
+        stack.append(-1)
+
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    max_length = max(max_length, i - stack[-1])
+
+        return max_length
+
+        
+# @lc code=end
+
