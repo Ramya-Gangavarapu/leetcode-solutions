d855c29e024895f92f7a2ea10bb9166b388d34a0
Ramya Gangavarapu <141222323+Ramya-Gangavarapu@users.noreply.github.com>
2023-11-04 08:58:02 +0530

    Add files via upload

---
+++ 44.wildcard-matching.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=44 lang=python3
+#
+# [44] Wildcard Matching
+#
+
+# @lc code=start
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        m, n = len(s), len(p)
+        
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
+        
+        
+        dp[0][0] = True
+        
+        
+        for j in range(1, n + 1):
+            if p[j - 1] == '*':
+                dp[0][j] = dp[0][j - 2]
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if p[j - 1] == s[i - 1] or p[j - 1] == '.':
+                    dp[i][j] = dp[i - 1][j - 1]
+                elif p[j - 1] == '*':
+                    dp[i][j] = dp[i][j - 2] or (dp[i - 1][j] and (s[i - 1] == p[j - 2] or p[j - 2] == '.'))
+        
+        return dp[m][n]
+
+        
+# @lc code=end
+
