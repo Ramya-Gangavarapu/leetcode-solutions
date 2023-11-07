e3c18ba8149e3e7e598fafa9100fba782a336366
Ramya Gangavarapu <141222323+Ramya-Gangavarapu@users.noreply.github.com>
2023-11-07 10:11:52 +0530

    Add files via upload

---
+++ 50.pow-x-n.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=50 lang=python3
+#
+# [50] Pow(x, n)
+#
+
+# @lc code=start
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1.0
+        elif n < 0:
+            x = 1 / x
+            n = -n
+
+        result = 1
+        while n > 0:
+            if n % 2 == 1:
+                result *= x
+            x *= x
+            n //= 2
+
+        return result
+
+        
+# @lc code=end
+
